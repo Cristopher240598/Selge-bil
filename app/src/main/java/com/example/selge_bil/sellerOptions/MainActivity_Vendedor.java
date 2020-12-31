dc703676455b080483d511ee89260fe8a8b92914
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-12-31 10:58:48 -0600

    Mostrando nombre, y correo en headerview y habilitando el logout vendedor

--- app/src/main/java/com/example/selge_bil/sellerOptions/MainActivity_Vendedor.java
+++ app/src/main/java/com/example/selge_bil/sellerOptions/MainActivity_Vendedor.java
@@ -1,10 +1,21 @@
 package com.example.selge_bil.sellerOptions;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.selge_bil.R;
+import com.example.selge_bil.activities.MainOption;
+import com.example.selge_bil.activities.vendedor.RegisterVendedorActivity;
+import com.example.selge_bil.providers.AuthProvider;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,10 +24,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import dmax.dialog.SpotsDialog;
+
 public class MainActivity_Vendedor extends AppCompatActivity
 {
 
     private AppBarConfiguration mAppBarConfiguration;
+    TextView tvUsuario, tvCorreo;
+    Button btnLogout;
+    AuthProvider mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,9 +41,33 @@ public class MainActivity_Vendedor extends AppCompatActivity
         setContentView(R.layout.activity_main_seller);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mAuth = new AuthProvider();
 
+        FirebaseApp.initializeApp(this);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        if(mAuth.existSession()) {
+            tvUsuario = headerView.findViewById(R.id.tV_usuario_nav);
+            tvUsuario.setText(mAuth.getName());
+            tvCorreo = headerView.findViewById(R.id.tvCorreoVendedor);
+            tvCorreo.setText(mAuth.getEmail());
+            btnLogout= headerView.findViewById(R.id.btnLogout);
+            btnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog mDialog;
+                    mDialog = new SpotsDialog.Builder().setContext(MainActivity_Vendedor.this).setMessage("Cerrando sesion").build();
+                    mDialog.show();
+                    mAuth.logout();
+                    mDialog.hide();
+                    startActivity(new Intent(MainActivity_Vendedor.this, MainOption.class));
+                    finish();
+                }
+            });
+
+        }
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -46,4 +86,5 @@ public class MainActivity_Vendedor extends AppCompatActivity
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
